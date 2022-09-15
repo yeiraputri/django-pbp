035e34216c8f068775aaeb902905260853824ecd
yeira.putri <yeira.putri@ui.ac.id>
2022-09-15 12:19:50 +0700

    first commit

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
+from django.http import HttpResponse
+from django.core import serializers
+
 
 # Create your views here.
 def show_wishlist(request):
@@ -10,3 +13,15 @@ def show_wishlist(request):
         'nama': 'Yeira'
     }
     return render(request, "wishlist.html",context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):    
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
