66b0c9e2e87755e5567d9c8470360b3216aee015
yeira.putri <yeira.putri@ui.ac.id>
2022-09-22 13:44:12 +0700

    first commit

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import show_xml
 from wishlist.views import show_json 
 from wishlist.views import show_json_by_id
 from wishlist.views import show_xml_by_id
+from wishlist.views import register 
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
     path('xml/<int:id>',show_xml_by_id, name='show_xml_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
